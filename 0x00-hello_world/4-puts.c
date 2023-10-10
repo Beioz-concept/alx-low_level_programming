909a39ac47debaa43f5e202da8d595deaed86374
Beioz-concept <doriswokoma8@gmail.com>
2023-10-09 19:30:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  * main - The entry point
  * 
  * Return: always 0
- * */
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzl`e");
+       	return (0);
 }
